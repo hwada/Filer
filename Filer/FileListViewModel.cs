c4817ecaf1f67f0c0a906b41e6e1ffa779ab5b33
wata <hk1324@gmail.com>
2022-10-01 20:49:35 +0900

    黒背景に変更

--- Filer/FileListViewModel.cs
+++ Filer/FileListViewModel.cs
@@ -13,16 +13,31 @@ using Reactive.Bindings.Extensions;
 
 namespace Filer
 {
+    /// <summary>
+    /// ファイルリストペインのVM
+    /// </summary>
     internal class FileListViewModel : BindableBase, IDisposable
     {
         private CompositeDisposable _disposables = new();
 
+        /// <summary>
+        /// 現在位置
+        /// </summary>
         public ReactiveProperty<string> Path { get; set; } = new("");
 
+        /// <summary>
+        /// 現在のディレクトリ下にあるディレクトリとファイルのリスト
+        /// </summary>
         public ReactiveCollection<FileItemViewModel> Files { get; set; } = new();
 
+        /// <summary>
+        /// 選択中のオブジェクト
+        /// </summary>
         public ReactiveProperty<FileItemViewModel> SelectedItem { get; set; } = new();
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         public FileListViewModel()
         {
             // TODO: 前回のフォルダを覚える
@@ -34,29 +49,50 @@ namespace Filer
 
         }
 
+        /// <summary>
+        /// オブジェクトを解放する
+        /// </summary>
         public void Dispose()
         {
             _disposables.Dispose();
         }
 
+        /// <summary>
+        /// キーダウンイベント
+        /// </summary>
+        /// <param name="e">押下されたキー</param>
         public void OnKeyDown(KeyEventArgs e)
         {
-            if (e.Key == Key.Enter)
+            switch (e.Key)
             {
-                if (SelectedItem.Value != null)
-                {
-                    if (SelectedItem.Value.Info is DirectoryInfo info)
+                case Key.Enter:
+                    if (SelectedItem.Value != null)
                     {
-                        MoveDirectory(info.FullName);
+                        if (SelectedItem.Value.Info is DirectoryInfo info)
+                        {
+                            MoveDirectory(info.FullName);
+                        }
                     }
-                }
+                    break;
+                case Key.Back:
+                    var current = new DirectoryInfo(Path.Value);
+                    if (current.Parent != null)
+                    {
+                        MoveDirectory(current.Parent.FullName);
+                    }
+                    break;
             }
         }
 
+        /// <summary>
+        /// ディレクトリを移動する
+        /// </summary>
+        /// <param name="dir">移動先のディレクトリ</param>
         private void MoveDirectory(string dir)
         {
             try
             {
+                var previousDir = Path.Value;
                 var children = new List<FileItemViewModel>();
                 var current = new DirectoryInfo(dir);
                 if (current.Parent != null)
@@ -80,8 +116,16 @@ namespace Filer
                 }
                 Files.Clear();
                 Files.AddRangeOnScheduler(children);
-                
-                SelectedItem.Value = children.First();
+
+                var prev = children.FirstOrDefault(x => x.Info.FullName == previousDir);
+                if (prev != null)
+                {
+                    SelectedItem.Value = prev;
+                }
+                else if (children.Count > 0)
+                {
+                    SelectedItem.Value = children.First();
+                }
             }
             catch
             {
